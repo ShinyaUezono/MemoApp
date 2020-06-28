55084c095f730f2fca23f4206fe5b890030c68e5
shinya.uezono <shinya.uezono@g.softbank.co.jp>
2020-06-28 23:23:48 +0900

    fix from dummy to db data of title, date, body

--- src/screens/MemoDetailScreen.js
+++ src/screens/MemoDetailScreen.js
@@ -4,21 +4,40 @@ import { StyleSheet, View, Text } from 'react-native';
 import CircleButton from '../elements/CircleButton';
 
 class MemoDetailScreen extends React.Component {
+  state = {
+    memo: {},
+  }
+
+// "componentDidMount" will cause TypeError even if I check null as below
+  UNSAFE_componentWillMount() {
+    const { params } = this.props.navigation.state;
+    this.setState({ memo: params.memo });
+  }
+
+  dateString(date) {
+    console.log(date.toDate().toISOString());
+    const str = date.toDate().toISOString();
+    return str.split('T')[0];
+  }
+
   render() {
+    const { memo } = this.state;
+    if (memo == null) { return null; }
+
     return (
       <View style={styles.container}>
         <View>
           <View style={styles.memoHeader}>
             <View>
-              <Text style={styles.memoHeaderTitle}>講座のアイデア</Text>
-              <Text style={styles.memoHeaderDate}>2020/06.15</Text>
+              <Text style={styles.memoHeaderTitle}>{memo.body.substring(0, 10)}</Text>
+              <Text style={styles.memoHeaderDate}>{this.dateString(memo.createdOn)}</Text>
             </View>
           </View>
         </View>
 
         <View style={styles.memoContents}>
-          <Text>
-            講座のアイデアです
+          <Text style={styles.memoBody}>
+            {memo.body}
           </Text>
         </View>
 
@@ -57,6 +76,10 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
     backgroundColor: '#fff',
     flex: 1,
+  },
+  memoBody: {
+    lineHeight: 22,
+    fontSize: 15,
   },
   editButton: {
     top: 75,
